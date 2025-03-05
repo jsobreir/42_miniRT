4591e89f72167916a1f38a969a9af10504abf3f4
Beatriz <beatrizoffline@gmail.com>
2025-03-05 17:24:57 +0000

    segfault and relinking corrected

--- src/windows/hooks.c
+++ src/windows/hooks.c
@@ -6,14 +6,21 @@
 /*   By: bpaiva-f <bpaiva-f@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 13:57:50 by bpaiva-f          #+#    #+#             */
-/*   Updated: 2025/02/26 13:57:51 by bpaiva-f         ###   ########.fr       */
+/*   Updated: 2025/03/05 17:24:26 by bpaiva-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
+static int free_exit(t_scene *scene)
+{
+	clean_exit(scene, NULL);
+	return (0);
+}
+
 void	setup_hooks(t_scene *scene)
 {
-	mlx_hook(scene->mlx_win, 17, 0, clean_exit, scene);
+	mlx_hook(scene->mlx_win, 17, 0, free_exit, scene);
 	mlx_key_hook(scene->mlx_win, handle_keys, scene);
 }
+
